7450786df106272315a713972fa1ee05d2918ecf
iskako01 <85966697+iskako01@users.noreply.github.com>
2022-05-09 21:37:36 +0200

    extends React.Component

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,63 +1,58 @@
+/* eslint-disable jsx-a11y/alt-text */
+import axios from "axios";
 import React from "react";
 import classes from "./Users.module.css";
+import userPhoto from "../../assets/img/noname.jpeg";
 
-const Users = (props) => {
-  if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        followed: false,
-        photoUrl:
-          "https://miro.medium.com/max/2560/1*Zea8TZCxKyLsIuNCjPv5UQ.jpeg",
-        fullname: "Alisher",
-        status: "I like it",
-        location: { city: "Almaty", country: "Kazakchstan" },
-      },
-      {
-        id: 2,
-        followed: true,
-        photoUrl:
-          "https://i.pinimg.com/originals/35/d9/ea/35d9eaf49c5a954b62e5930f56c452b5.jpg",
-        fullname: "Toshka",
-        status: "Tbilisi",
-        location: { city: "Almaty", country: "Kazakchstan" },
-      },
-    ]);
+class Users extends React.Component {
+  getUsers = () => {
+    if (this.props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => {
+          this.props.setUsers(response.data.items);
+        });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        {this.props.users.map((u) => (
+          <div className={classes.container} key={u.id}>
+            <div className={classes.follow}>
+              <div>
+                <img
+                  src={u.photos.small !== null ? u.photos.small : userPhoto}
+                  className={classes.avatar}
+                />
+              </div>
+
+              <div className={classes.btn}>
+                {u.followed ? (
+                  <button onClick={() => this.props.unfollow(u.id)}>Unfollow</button>
+                ) : (
+                  <button onClick={() => this.props.follow(u.id)}>Follow</button>
+                )}
+              </div>
+            </div>
+
+            <div className={classes.info}>
+              <div className="">
+                <div className="">{u.name}</div>
+                <div className="">{u.status}</div>
+              </div>
+
+              <div className="">
+                <div className="">{"u.location.country"}</div>
+                <div className="">{"u.location.city"}</div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
-
-  return (
-    <div>
-      {props.users.map((u) => (
-        <div className={classes.container} key={u.id}>
-          <div className={classes.follow}>
-            <div>
-              <img src={u.photoUrl} className={classes.avatar} />
-            </div>
-
-            <div className={classes.btn}>
-              {u.followed ? (
-                <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
-              ) : (
-                <button onClick={() => props.follow(u.id)}>Follow</button>
-              )}
-            </div>
-          </div>
-
-          <div className={classes.info}>
-            <div className="">
-              <div className="">{u.fullname}</div>
-              <div className="">{u.status}</div>
-            </div>
-
-            <div className="">
-              <div className="">{u.location.country}</div>
-              <div className="">{u.location.city}</div>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
+}
 
 export default Users;
--- src/redux/redusers/usersReduser.js
+++ src/redux/redusers/usersReduser.js
@@ -4,24 +4,24 @@ const SET_USERS = "SET_USERS";
 
 let initialState = {
   users: [
-    {
-      id: 1,
-      followed: false,
-      photoUrl:
-        "https://miro.medium.com/max/2560/1*Zea8TZCxKyLsIuNCjPv5UQ.jpeg",
-      fullname: "Alisher",
-      status: "I like it",
-      location: { city: "Almaty", country: "Kazakchstan" },
-    },
-    {
-      id: 2,
-      followed: true,
-      photoUrl:
-        "https://i.pinimg.com/originals/35/d9/ea/35d9eaf49c5a954b62e5930f56c452b5.jpg",
-      fullname: "Toshka",
-      status: "Tbilisi",
-      location: { city: "Almaty", country: "Kazakchstan" },
-    },
+    // {
+    //   id: 1,
+    //   followed: false,
+    //   photoUrl:
+    //     "https://miro.medium.com/max/2560/1*Zea8TZCxKyLsIuNCjPv5UQ.jpeg",
+    //   fullname: "Alisher",
+    //   status: "I like it",
+    //   location: { city: "Almaty", country: "Kazakchstan" },
+    // },
+    // {
+    //   id: 2,
+    //   followed: true,
+    //   photoUrl:
+    //     "https://i.pinimg.com/originals/35/d9/ea/35d9eaf49c5a954b62e5930f56c452b5.jpg",
+    //   fullname: "Toshka",
+    //   status: "Tbilisi",
+    //   location: { city: "Almaty", country: "Kazakchstan" },
+    // },
   ],
 };
 
