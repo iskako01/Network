9045875eeaa1672742c071e6dc97744acfd2fc62
iskako01 <85966697+iskako01@users.noreply.github.com>
2022-05-27 19:03:11 +0200

    useEffect

--- src/components/profile/ProfileStatus.jsx
+++ src/components/profile/ProfileStatus.jsx
@@ -1,36 +1,22 @@
-import React from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const ProfileStatus = (props) => {
-  //   state = {
-  //     editMode: false,
-  //     status: this.props.status,
-  //   };
+  useEffect(() => {
+    setStatus(props.status);
+  }, [props.status]);
 
-  //   activateEditMode = () => {
-  //     this.setState({ editMode: true });
-  //   };
-  //   deactivateEditMode = () => {
-  //     this.setState({ editMode: false });
-  //     this.props.updateStatusProfile(this.state.status);
-  //   };
-  //   onStatusChange = (e) => {
-  //     this.setState({ status: e.currentTarget.value });
-  //   };
-  //   componentDidUpdate(prevProps, prevState) {
-  //     if (prevProps.status !== this.props.status) {
-  //       this.setState({ status: this.props.status });
-  //     }
-  //   }
   let [editMode, setEditMode] = useState(false);
   let [status, setStatus] = useState(props.status);
+
   const activateEditMode = () => {
     setEditMode(true);
   };
+
   const deactivateEditMode = () => {
     setEditMode(false);
     props.updateStatusProfile(status);
   };
+
   const onStatusChange = (e) => {
     setStatus(e.currentTarget.value);
   };
